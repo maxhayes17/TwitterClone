d0bd834b2d392cf9101f6cd1b184c1e8adab57b4
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-10 21:10:29 -0400

    Create login controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -30,4 +30,29 @@ export const register = async(req, res) => {
         // Send unsuccessful status code with error message if error occurs
         res.status(500).json({error: err.message})
     }
-}
+};
+
+export const login = async(req, res) => {
+    try {
+        // De-structure request body
+        const {
+            email,
+            password
+        } = req.body;
+        
+        // Search DB for user with unique email
+        const user = User.findOne({email: email});
+
+        // If user does not exist, or incorrect password was entered, return 400 (Client error) status code
+        if (!user || !(await bcrypt.compare(password, user.password))) 
+            return res.status(400);
+
+        // Otherwise... User exists and has logged in with correct credentials
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        // Remove password from user object so it is not included to response
+        delete user.pw_hash;
+        res.status(200).json({token, user})
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
