52debda0c03f344a3eb5a214b6638f65d25d0a5b
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-13 13:40:43 -0400

    Add routes for feed, login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+import { createBrowserRouter, Navigate, Routes, Route, RouterProvider } from "react-router-dom";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import Login from "./pages/login";
+import Feed from "./pages/feed";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Create array of route objects for website
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/feed",
+      element: <Feed />
+    },
+  ]);
+
   return (
-    <div>
-      <h1>TwitterClone</h1>
-    </div>
+      <div>
+        <RouterProvider router={router}/>
+      </div>
   );
 }
 
