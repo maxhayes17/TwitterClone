432bd7f71bcc1644d501c66b2118b64e56215726
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-13 13:41:01 -0400

    Create login page

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+
+function Login(){
+    // Default page type to be login page
+    return(
+        <div>
+            <LoginForm />
+        </div>
+    );
+}
+export default Login;
