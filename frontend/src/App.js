8f9117d6407a66486224815976217fcafe829df9
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-13 15:36:58 -0400

    Add profile route, navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import { createBrowserRouter, Navigate, Routes, Route, RouterProvider } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+
+
 import Login from "./pages/login";
 import Feed from "./pages/feed";
+import Profile from "./pages/profile";
+
+import Navbar from "./components/Navbar";
+
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Create boolean variable to determine whether user is authorized
+  const auth = Boolean(useSelector((state) => state.token));
 
   // Create array of route objects for website
   const router = createBrowserRouter([
@@ -16,12 +25,17 @@ function App() {
     },
     {
       path: "/feed",
-      element: <Feed />
+      element: auth ? <Feed /> : <Navigate to="/"/>
     },
+    {
+      path: "/profile",
+      element: auth ? <Profile /> : <Navigate to="/"/>
+    }
   ]);
 
   return (
       <div>
+        <Navbar />
         <RouterProvider router={router}/>
       </div>
   );
