fc587ace0e6df8d6c77cca86cad491ac1df98ac6
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-17 13:23:18 -0400

    Update user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,20 +5,47 @@ const userSchema = new mongoose.Schema(
         username: {
             type: String,
             required: true,
+            unique: true,
             min: 3,
-            max: 25,
-            unique: true
+            max: 25
         },
         email: {
             type: String,
             required: true,
-            max: 50,
-            unique: true
+            unique: true,
+            max: 50
         },
         pw_hash: {
             type: String,
             required: true
         },
+        name: {
+            type: String,
+            max: 50
+        },
+        bio: {
+            type: String,
+            max: 160
+        },
+        location: {
+            type: String,
+            max: 30
+        },
+        website: {
+            type: String,
+            max: 100
+        },
+        followers: [{
+            type: ObjectId,
+            ref: "User", 
+            required: true
+        }],
+        following: [{
+            type: ObjectId,
+            ref: "User", 
+            required: true
+        }],
+
         // posts: [{
         //     author: String, 
         //     body: String, 
