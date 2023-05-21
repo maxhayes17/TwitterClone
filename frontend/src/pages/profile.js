b436aaf3ad8b69654b38866900deaa61d9975e02
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-21 15:09:13 -0400

    Update profile.js

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -18,12 +18,12 @@ function Profile({edit}){
 
     const [user, setUser] = useState(null);
     const [posts, setPosts] = useState(null);
-    const userId = useParams();
+    const {id} = useParams();
     
     const token = useSelector((state) => state.token);
 
     const getUserInfo = () => {
-        fetch("http://localhost:3001/user/" + userId.id, {
+        fetch("http://localhost:3001/user/" + id, {
             method: "GET",
             headers: {
                 Authorization: "Bearer " + token
@@ -37,7 +37,7 @@ function Profile({edit}){
         .catch((err) => console.log(err));
     };
     const getUserPosts = () => {
-        fetch("http://localhost:3001/posts/" + userId.id + "/posts", {
+        fetch("http://localhost:3001/posts/" + id + "/posts", {
             method: "GET",
             headers: {
                 Authorization: "Bearer " + token
@@ -54,10 +54,8 @@ function Profile({edit}){
     return(
         <div>
             <Navbar />
-            {edit && <ProfileForm userId={userId} token={token} setUser={setUser}/>}
+            {edit && <ProfileForm userId={id} token={token} setUser={setUser}/>}
             {user && posts && <ProfileCard user={user} posts={posts}/>}
-            {/* {posts && posts.map(({_id, author, body, date}) => 
-            <Post key={_id} body={body} author={author} date={date} userProfile={user}/>)} */}
             <ExploreCard />
         </div>
     );
