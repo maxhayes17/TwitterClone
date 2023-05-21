4cc41a70fc6162ba38f341208dd68735c90b118d
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-21 15:07:42 -0400

    create getPostInfo route

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,6 +1,16 @@
 import Post from "../models/Post.js";
 import User from "../models/User.js";
 
+export const getPostInfo = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const post = await Post.findById(id);
+
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(404).json({error: err.message});
+    }
+}
 
 export const getPosts = async (req, res) => {
     try {
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,13 @@
 import express from "express";
 
-import { getPosts, createPost, getUserPosts } from "../controllers/post.js";
+import { getPosts, createPost, getUserPosts, getPostInfo } from "../controllers/post.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getPosts);
 router.get("/:id/posts", verifyToken, getUserPosts);
+router.get("/:id", verifyToken, getPostInfo);
 router.post("/compose", verifyToken, createPost);
 
 export default router;
