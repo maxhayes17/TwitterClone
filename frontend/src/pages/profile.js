186a56c6221fa75bb321c71e30b594a72c5f0b2e
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-05-22 20:30:07 -0400

    put profileCard right into profile page

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 
 import ProfileCard from "../components/ProfileCard";
@@ -9,17 +9,21 @@ import ExploreCard from "../components/ExploreCard";
 import ProfileForm from "../components/ProfileForm";
 import Post from "../components/Post";
 
+
 function Profile({edit}){
 
     useEffect(() => {
         getUserInfo();
-        // getUserPosts();
     }, []);
 
     const [user, setUser] = useState(null);
     const [posts, setPosts] = useState(null);
     const {id} = useParams();
     
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const currentUser = useSelector((state) => state.user);
     const token = useSelector((state) => state.token);
 
     const getUserInfo = () => {
@@ -45,17 +49,69 @@ function Profile({edit}){
         })
         .then((res) => res.json())
         .then((data) => {
-            // console.log(data);
             setPosts(data);
         })
         .catch((err) => console.log(err));
-    }
+    };
+
+    const addFollower = () => {
+        fetch("http://localhost:3001/user/" + user._id + "/follow", {
+            method: "PATCH",
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                followerId: currentUser._id,
+                followeeId: user._id
+            })
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            dispatch(
+                setUser({user: data.follower})
+            );
+            navigate("/profile/" + data.followee._id);
+        })
+        .catch((err) => console.log(err));
+    };
 
     return(
         <div>
             <Navbar />
-            {edit && <ProfileForm userId={id} token={token} setUser={setUser}/>}
-            {user && posts && <ProfileCard user={user} posts={posts}/>}
+            {user && <div>
+            {edit && (user._id == currentUser._id) && <ProfileForm token={token} setUser={setUser}/>}
+            
+                {posts && <div className="mainCard">
+                    <a onClick={() => navigate("/home")}><h1>{"<--"}</h1></a>
+
+                    <div style={{padding: "0 20px", textAlign:"inherit"}}>
+                        <div className="inline">
+                            <h3>{user.name}</h3>
+                            {(user._id == currentUser._id ) 
+                            ? 
+                                <a onClick={() => navigate("/profile/" + user._id + "/edit")} 
+                                    className="button-round" 
+                                        style={{border: "1px solid #333639"}}>Edit profile</a>
+                            :
+                                <a onClick={addFollower} className="button-round" id="white">{currentUser.following.includes(user._id) ? "Following" : "Follow"}</a>
+                            }
+                        </div>
+                        <p style={{opacity:"70%"}}>@{user.username}</p>
+                        <p>{user.bio}</p>
+                        <p style={{opacity:"70%"}}>{ user.location ? user.location : ""} Joined {user.createdAt.slice(0,10)}</p>
+                        <p>
+                            <a onClick={() => navigate("/profile/" + user._id + "/following")} style={{fontWeight:"bold"}}>{user.following.length}</a> Following 
+                            <a onClick={() => navigate("/profile/" + user._id + "/followers")} style={{fontWeight:"bold"}}> {user.followers.length}</a> Followers
+                        </p>
+                        <p>Posts| Replies | Media | Likes</p>
+                    </div>
+                    {posts && posts.map(({_id, author, body, date}) => 
+                    <Post key={_id} id={_id} body={body} author={author} date={date} userProfile={user}/>)}
+                </div>}
+            {/* {posts && <ProfileCard user={user} posts={posts}/>} */}
+            </div>}
             <ExploreCard />
         </div>
     );
