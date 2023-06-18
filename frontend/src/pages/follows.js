9485c63e6095ea3c4e71b868b24a3673f7260830
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-06-17 21:18:38 -0400

    Add back button arrow

--- frontend/src/pages/follows.js
+++ frontend/src/pages/follows.js
@@ -66,7 +66,14 @@ function Follows({isFollowers}){
 
                 <div className="vertical-nav">
                     <div className="inline">
-                        <a onClick={() => navigate(-1)} style={{paddingRight:"20px"}}><h2>{"<"}</h2></a>
+                        <a onClick={() => navigate(-1)} style={{paddingRight:"20px"}}>
+                            <h2>
+                                {/* back button  */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                                </svg> 
+                            </h2>
+                        </a>
                         <div className="vertical-stack">
                             <h3>{user.name}</h3>
                             <p style={{opacity:"70%"}}>@{user.username}</p>
--- frontend/src/pages/post.js
+++ frontend/src/pages/post.js
@@ -81,7 +81,18 @@ function ViewPost(){
             {post && <div className="mainCard">
 
                 <div className="vertical-nav">
-                    <h2><a onClick={() => navigate(-1)}>{"<"}</a> Post</h2>
+                    <div className="inline">
+                        <a onClick={() => navigate(-1)}>
+                            <h2>
+                                {/* back button  */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                                </svg> 
+                            </h2>
+                        </a>
+                        <h2 style={{marginBlock: "auto", marginLeft:"10px"}}>Post</h2>
+
+                    </div>
                 </div>
             
                 <Post key={post._id} id={post._id} author={post.author} body={post.body} createdAt={post.createdAt} likes={post.likes} replies={post.replies}/>
