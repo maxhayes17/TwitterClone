f846426415d91405542c47fde4618564981163a2
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-06 18:41:54 -0400

    Add hashtag search

--- frontend/src/components/ExploreCard.js
+++ frontend/src/components/ExploreCard.js
@@ -8,39 +8,48 @@ export default function ExploreCard(){
     const users = useSelector((state) => state.users);
     const navigate = useNavigate();
     const [results, setResults] = useState([]);
+    const [search, setSearch] = useState(null);
 
     useEffect(() => {
         setResults([]);
     }, []);
 
     return(
-        <div className="exploreCard">
-            <div className="search" style={{borderBottom: results && results.length > 0 ? "1px solid #333639" : "none"}}>
+        <div className="w-1/3 h-full sticky top-0 flex flex-col border-l border-onyx bg-black">
+            <div className="mt-5">
                 <form>
-                    <input type="text" placeholder="Search TwitClone" 
+                    <input className="w-full mb-4 px-5 py-3 rounded-full"
+                        type="text" placeholder="Search TwitClone" 
                         onChange={e => {
                             let result = users.filter((user) => {
                                 return user.username.toLowerCase().includes(e.target.value.toLowerCase())
                             });
                             setResults(result);
+                            setSearch(e.target.value);
                         }}                 
                     />
                 </form>
+                {search && <div className="flex flex-col last:border-b last:border-onyx">
                 {results && results.length < users.length 
                 && results.map(({_id, name, username, bio, picture_path}) =>
-                        <div className="profile-list-element" key={_id} onClick={() => {navigate(`/profile/${_id}`)}}>
-                            <div className="inline"> 
-                                <div className="image-avatar">
-                                    <img src={`http://localhost:3001/uploads/${picture_path ? picture_path : "image-avatar-blank.png"}`}></img>
-                                </div>
-                                <div className="vertical-stack">
-                                    <a onClick={() => {navigate(`/profile/${_id}`)}} style={{fontWeight:"bold"}}>{name}</a>
-                                    <p style={{opacity:"70%"}}>@{username}</p>
-                                    <p>{bio}</p>
-                                </div>
+
+                    <div className="py-2 px-4 bg-black hover:bg-black-semitransparent hover:cursor-pointer text-left" key={_id} onClick={() => {navigate(`/profile/${_id}`)}}>
+                        <div className="flex flex-row">
+                            <div className="w-fit shrink-0 mr-3">
+                                <img className="w-14 h-14 rounded-full object-cover mx-auto" src={`http://localhost:3001/uploads/${picture_path ? picture_path : "image-avatar-blank.png"}`}></img>
+                            </div>
+
+                            <div className="flex flex-col text-left">
+                                <a className="text-md text-white font-bold hover:underline hover:underline-offset-2 hover:decoration-white hover:opacity-100">{name}</a>
+                                <a className="text-md text-neutral-400">@{username}</a>
+                                <p>{bio}</p>
                             </div>
                         </div>
+                    </div>
                 )}
+                <div className="py-2 px-4 bg-black hover:bg-black-semitransparent hover:cursor-pointer text-left text-bold text-twitter-blue"
+                    onClick={() => {navigate(`/explore/${search}`)}}>#{search}</div>
+                </div>}
             </div>
         </div>
     )
