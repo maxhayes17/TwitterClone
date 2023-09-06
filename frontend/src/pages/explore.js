2fe817c482dc972485206b9c2f171a8f043a124f
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-06 19:45:33 -0400

    Update explore.js

--- frontend/src/pages/explore.js
+++ frontend/src/pages/explore.js
@@ -29,31 +29,34 @@ function Explore(){
         .then((res) => res.json())
         .then((data) => {
             setFeed(data);
+            console.log(data);
             feedRef.current.focus();
         })
         .catch((err) => console.log(err));
     }
 
     return(
-        <div>
+        <div className="flex flex-row w-screen h-screen">
             <Navbar />
-            <div className="mainCard">
-                <div className="vertical-nav">
-                    <h2>Explore</h2>
-                {tag ? <div className="btn-group">
-                        <button ref={feedRef}>Latest</button>
-                        <button>Popular</button>
-                    </div> 
-                    : <div className="btn-group">
-                        <button>For you</button>
-                        <button>Trending</button>
-                        <button>News</button>
-                        <button>Sports</button>
-                    </div>
-                }
-                </div>
+            <div className="w-5/12 h-full overflow-auto bg-black text-left">
 
-                {feed && feed.map( ({_id, author, body, createdAt, likes, replies, picture_path}) => <Post key={_id} id={_id} author={author} body={body} createdAt={createdAt} likes={likes} replies={replies} picture_path={picture_path}/>)}
+                <div className="w-full sticky top-0 z-10 bg-black-rgba backdrop-blur-sm border-b border-onyx">
+                    
+                    {feed && tag
+                        ? <div className="flex flex-col justify-around p-5 text-left">
+                            <h1 className="text-xl font-extrabold">#{tag}</h1>
+                            <p className="text-sm text-neutral-400">{feed.length} {feed.length == 1 ? "Post" : "Posts"}</p>
+                        </div>
+                        : <h1 className="p-5 text-left text-xl font-bold">Explore</h1>
+                    }
+                    <div className="flex flex-row justify-around">
+                        <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black" ref={feedRef}>Latest</button>
+                        <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black">Popular</button>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    {feed && feed.map( ({_id, author, body, createdAt, likes, replies, picture_path}) => <Post key={_id} id={_id} author={author} body={body} createdAt={createdAt} likes={likes} replies={replies} picture_path={picture_path}/>)}
+                </div>
             </div>
             <ExploreCard />
         </div>
