abb407c5ffae37cafa71207d98466b4312cc5b8e
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-06 14:49:39 -0400

    Add tailwind for profile page

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -4,6 +4,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setUserInfo } from "../state";
 
+import reactStringReplace from 'react-string-replace';
+
 import ExploreCard from "../components/ExploreCard";
 import Post from "../components/Post";
 
@@ -121,28 +123,79 @@ function Profile(){
     };
 
     return(
-        <div>
+        <div className="flex flex-row bg-blue-500 w-screen h-screen">
             <Navbar />
-            {user && <div>
-                {posts && <div className="mainCard">
-                    <div className="vertical-nav">
-                        <div className="inline">
-                            <a onClick={() => navigate(-1)} style={{paddingRight:"20px"}}>
-                                <h2>
-                                    {/* back button  */}
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                                    </svg>
-                                </h2>
-                            </a>
-                            <div className="vertical-stack">
-                                <h3>{user.name}</h3>
-                                <p style={{opacity:"70%"}}>{user.posts.length} {user.posts.length == 1 ? "Post" : "Posts"}</p>
+            {user && <div className="w-5/12 h-full overflow-auto bg-black text-left">
+                {posts && <div>
+
+                    <div className="w-full sticky top-0 z-10 py-1 bg-black-rgba backdrop-blur-sm">
+                        <div className="flex flex-row space-x-5">
+                            <div onClick={() => navigate(-1)} className="ml-1 my-auto">
+                                {/* back button  */}
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 p-2 rounded-full hover:bg-onyx hover:bg-opacity-50 hover:cursor-pointer">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                                </svg>
+                            </div>
+                            <div className="flex flex-col justify-around">
+                                <h1 className="text-left text-xl font-extrabold">{user.name}</h1>
+                                <p className="my-auto text-sm opacity-70">{user.posts.length} {user.posts.length == 1 ? "Post" : "Posts"}</p>
                             </div>
                         </div>
                     </div>
 
-                    <div style={{padding: "0 20px", textAlign:"inherit"}}>
+                    <div className="flex flex-col text-left mx-5 mt-8">
+                        <div className="flex flex-row justify-between items-end ">
+                            <div className="w-fit shrink-0 mr-3">
+                                <img className="w-32 h-32 rounded-full object-cover mx-auto hover:cursor-pointer hover:opacity-80 border-4 border-onyx" src={`http://localhost:3001/uploads/${user.picture_path ? user.picture_path : "image-avatar-blank.png"}`}></img>
+                            </div>
+                            {(user._id == currentUser._id ) 
+                            ? 
+                                <div onClick={() => navigate(`/profile/${user._id}/edit`)} 
+                                    className="bg-black px-4 py-2 rounded-full text-white font-bold border border-onyx hover:opacity-70 hover:cursor-pointer" >Edit profile</div>
+                            :
+                                <div onClick={addFollower} 
+                                    className={currentUser.following.includes(user._id) 
+                                        ? "bg-black px-4 py-3 rounded-full text-white font-bold border border-onyx hover:opacity-70 hover:cursor-pointer" 
+                                        : "bg-white px-4 py-3 rounded-full text-black font-bold hover:opacity-70 hover:cursor-pointer"}>
+                                    {currentUser.following.includes(user._id) ? "Following" : "Follow"}
+                                </div>
+                            }
+                        </div>
+                        <div className="flex flex-col my-3 text-left">
+                            <h1 className="text-xl font-extrabold">{user.name}</h1>
+                            <p className="opacity-70 text-sm">@{user.username}</p>
+                        </div>
+                        <p>
+                            {reactStringReplace(user.bio, /#(\w+)/g, (match, i) => (
+                                <a key={i} 
+                                className="text-twitter-blue hover:underline hover:underline-offset-2 hover:opacity-100"
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    navigate(`/explore/${match}`)}}
+                                >#{match}</a>
+                            ) )}
+                        </p>
+                        <div className="flex flex-row items-start space-x-3 opacity-60">
+                            {user.location && <p>{user.location}</p>}
+                            <p>Joined {user.createdAt.slice(0,10)}</p>
+                        </div>
+                        <div className="flex flex-row space-x-4">
+                            <p>
+                                <a onClick={() => navigate(`/profile/${user._id}/following`)} className="font-extrabold text-white opacity-100">{user.following.length}</a> Following
+                            </p>
+                            <p>
+                                <a onClick={() => navigate(`/profile/${user._id}/followers`)} className="font-extrabold opacity-100"> {user.followers.length}</a> Followers
+                            </p>
+                        </div>
+                        <div className="flex flex-row justify-around mt-5">
+                            <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black" onClick={getUserPosts} ref={feedRef}>Posts</button>
+                            <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black" onClick={getUserReplies}>Replies</button>
+                            <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black" onClick={getUserMedia}>Media</button>
+                            <button className="text-md font-bold p-5 focus:border-b-twitter-blue focus:border-b-4 hover:bg-raisin-black" onClick={getUserLiked}>Likes</button>
+                        </div>
+                    </div>
+
+                    {/* <div style={{padding: "0 20px", textAlign:"inherit"}}>
 
                         <div className="inline">
                             <div className="image-avatar" style={{width:"75px", height:"75px"}}>
@@ -179,13 +232,13 @@ function Profile(){
                             <button onClick={getUserLiked}>Likes</button>
                         </div>}
 
-                    </div>
+                    </div> */}
                     {posts && posts.map(({_id, author, body, root, createdAt, likes, replies, picture_path}) => 
                         <Post key={_id} id={_id} body={body} author={author} root={root} createdAt={createdAt} likes={likes} replies={replies} picture_path={picture_path}/>)
                     }
                 </div>}
             </div>}
-            <ExploreCard />
+            {/* <ExploreCard /> */}
         </div>
     );
 }
