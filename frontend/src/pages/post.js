8e26ae6aad2887836232aa8af2d059cab287cb6e
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-06 19:45:26 -0400

    Add tailwind for single post view

--- frontend/src/pages/post.js
+++ frontend/src/pages/post.js
@@ -77,38 +77,37 @@ function ViewPost({reply}){
     }
 
     return(
-        <div>
+        <div className="flex flex-row w-screen h-screen">
             <Navbar />
-            {post && <div className="mainCard">
-
-                <div className="vertical-nav">
-                    <div className="inline">
-                        <a onClick={() => navigate(-1)}>
-                            <h2>
-                                {/* back button  */}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                                </svg> 
-                            </h2>
-                        </a>
-                        <h2 style={{marginBlock: "auto", marginLeft:"10px"}}>Post</h2>
-
+            {post && <div className="w-5/12 h-full overflow-auto bg-black text-left">
+                <div className="w-full sticky top-0 z-10 py-1 bg-black-rgba backdrop-blur-sm">
+                    <div className="flex flex-row space-x-5">
+                        <div onClick={() => navigate(-1)} className="ml-1 my-auto">
+                            {/* back button  */}
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 p-2 rounded-full hover:bg-raisin-black hover:cursor-pointer">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                            </svg>
+                        </div>
+                        <h1 className="my-auto text-xl font-extrabold">Post</h1>
                     </div>
                 </div>
-            
-                <Post key={post._id} id={post._id} author={post.author} body={post.body} createdAt={post.createdAt} likes={post.likes} replies={post.replies} tags={post.tags} picture_path={post.picture_path}/>
 
-                <div className="inline" style={{paddingBottom:"10px", borderBottom: "1px solid #333639"}}>
-                    <p><a style={{fontWeight: "bold", marginLeft:"10px"}}>{post.likes.length}</a> {post.likes.length == 1 ? "like" : "likes"}</p>
+                <div className="flex flex-col">
+                    <Post key={post._id} id={post._id} author={post.author} body={post.body} createdAt={post.createdAt} likes={post.likes} replies={post.replies} tags={post.tags} picture_path={post.picture_path}/>
+
+                    <div className="border-b border-onyx text-left px-5 py-3">
+                        <p><a className="font-bold">{post.likes.length}</a> {post.likes.length == 1 ? "like" : "likes"}</p>
+                    </div>
+
+                    <form onSubmit={composeReply} className="flex flex-row w-full my-5 justify-around">
+                        <input className="w-3/4 my-auto" placeholder="Reply to this post!" name="body" autoComplete="off" type="text" ref={replyRef}/>
+                        <button type="submit" className="w-fit h-fit my-auto px-4 py-2 rounded-full text-white font-bold bg-twitter-blue hover:opacity-70 hover:cursor-pointer">Reply</button>
+                    </form>
+
+                    {replies && replies.map( ({_id, author, body, createdAt, likes, replies, picture_path}) => 
+                        <Post key={_id} id={_id} author={author} body={body} createdAt={createdAt} likes={likes} replies={replies} picture_path={picture_path}/>)
+                    }
                 </div>
-                <form onSubmit={composeReply} className="replyForm">
-                    <input placeholder="Reply to this post!" name="body" autoComplete="off" type="text" ref={replyRef}/>
-                    <button type="submit" className="button-round" id="blue">Reply</button>
-                </form>
-
-                {replies && replies.map( ({_id, author, body, createdAt, likes, replies, picture_path}) => 
-                    <Post key={_id} id={_id} author={author} body={body} createdAt={createdAt} likes={likes} replies={replies} picture_path={picture_path}/>)
-                }
             </div>}
             <ExploreCard />
         </div>
