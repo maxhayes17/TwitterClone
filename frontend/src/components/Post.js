1d9e320d2f6cea1057d519c0ed1e60554ecf1630
maxhayes17 <52089291+maxhayes17@users.noreply.github.com>
2023-09-05 21:41:37 -0400

    Add tailwind to post component

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -63,75 +63,70 @@ export default function Post({id, author, body, root, createdAt, userProfile, li
 
 
     return(
-        <div>
-            {user && <div className="post" onClick={() => navigate(`/post/${(root & root != id ? root : id)}`)}>
-                <div className="inline">
-                    <div className="image-avatar">
-                        <img src={`http://localhost:3001/uploads/${user.picture_path ? user.picture_path : "image-avatar-blank.png"}`}></img>
+        <div className="">
+            {user && <div className="flex flex-row px-4 pt-5 border border-onyx hover:bg-raisin-black hover:cursor-pointer" onClick={() => navigate(`/post/${(root & root != id ? root : id)}`)}>
+                    <div className="w-fit shrink-0 mr-3">
+                        <img className="w-14 h-14 rounded-full object-cover" src={`http://localhost:3001/uploads/${user.picture_path ? user.picture_path : "image-avatar-blank.png"}`}></img>
                     </div>
-                    <a onClick={(event) => {
-                        // So click on elements inside div don't act as clicks on div
-                        event.stopPropagation()
-                        navigate(`/profile/${user._id}`)}} style={{fontWeight:"bold", marginBlock:"auto"}}>{user.name}</a>
-                    <p style={{opacity:"70%"}}>@{user.username} • {createdAt.slice(0,10)}</p>
-                </div>
-                
+                    <div className="flex flex-col w-full">
 
-                {/* use reactStringReplace to find/replace hashtags with links */}
-                <p>
-                    {reactStringReplace(body, /#(\w+)/g, (match, i) => (
-                        <a key={i} 
-                        className="hashtag"
-                        onClick={(event) => {
-                            event.stopPropagation();
-                             navigate(`/explore/${match}`)}}
-                        >#{match}</a>
-                    ) )}
-                </p>
-                {picture_path && <div style={{width:"100px"}}>
-                    <img src={`http://localhost:3001/uploads/${picture_path}`} style={{width:"600px"}}></img>
-
-                </div>
-                }
-
-                {currentUser && <div className="inline">
-
-
-                    {/* like Button */}
-                    <a onClick={(event) => {
-                        event.stopPropagation()
-                        addLike();
-                    }} className="likeBtn"
-                    style={{color: currentUser.liked_posts.includes(id) ? "#f91880" : "#ffffffba"}}
-                    >
-                        <div className="inline">
-                            {currentUser.liked_posts.includes(id) 
-                            ?
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                            </svg> 
-                            :
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                            </svg>
-                            }
-                            <p style={{marginBlock:"auto", marginRight:"25px"}}>{likes && likes.length}</p>
+                        <div className="flex flex-row">
+                            <a onClick={(event) => {
+                            // So click on elements inside div don't act as clicks on div
+                            event.stopPropagation()
+                            navigate(`/profile/${user._id}`)}} className="text-md font-bold mr-3 my-auto" >{user.name}
+                            </a>
+                            <p className="opacity-70 my-auto">@{user.username} • {createdAt.slice(0,10)}</p>
                         </div>
-                    </a>
-                    {/* comment Button */}
-                    <a className="commentBtn" onClick={(event) => {
-                        event.stopPropagation()
-                        navigate(`/post/${(root & root != id ? root : id)}/reply`)
-                    }}>
-                        <div className="inline">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
-                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                            </svg>
-                            <p style={{marginBlock:"auto", marginRight:"25px"}}>{replies && replies.length}</p>
-                        </div>
-                    </a>
-                </div>}
 
+                        <p className="text-left">
+                            {reactStringReplace(body, /#(\w+)/g, (match, i) => (
+                                <a key={i} 
+                                className="text-twitter-blue hover:underline hover:underline-offset-2 hover:opacity-100"
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    navigate(`/explore/${match}`)}}
+                                >#{match}</a>
+                            ) )}
+                        </p>
+                        
+                        {picture_path && 
+                            <img className="max-w-full mt-2 rounded-xl border border-onyx"src={`http://localhost:3001/uploads/${picture_path}`}></img>
+                        }
+
+                        {currentUser && <div className="flex flex-row justify-start space-x-6 my-2">
+                            {/* like Button */}
+                            <div className="hover:fill-pink-600" onClick={(event) => {
+                                event.stopPropagation()
+                                addLike();
+                            }} style={{color: currentUser.liked_posts.includes(id) ? "#f91880" : "#ffffffff"}}>
+                                <div className="flex flex-row items-center">
+                                    {currentUser.liked_posts.includes(id) 
+                                    ?
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                    </svg> 
+                                    :
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                                    </svg>
+                                    }
+                                    <p className="my-auto ml-2">{likes && likes.length}</p>
+                                </div>
+                            </div>
+                            {/* comment Button */}
+                            <div onClick={(event) => {
+                                event.stopPropagation()
+                                navigate(`/post/${(root & root != id ? root : id)}/reply`)}}>
+                                <div className="flex flex-row items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+                                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                                    </svg>
+                                    <p className="my-auto ml-2">{replies && replies.length}</p>
+                                </div>
+                            </div>
+                        </div>}
+                    </div>
             </div>}
         </div>
     );
